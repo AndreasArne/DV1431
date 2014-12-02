0529cf3ee4d12f1ca63a1cf71213e27c92b3ec21
Mihai Musatoiu <mihai.musatoiu@gmail.com>
2014-12-02 10:33:52 +0100

    added functionality to Question and Quiz classes

--- Quizzard/app/src/main/java/se/bth/students/quizzard/Question.java
+++ Quizzard/app/src/main/java/se/bth/students/quizzard/Question.java
@@ -4,4 +4,19 @@ package se.bth.students.quizzard;
  * Created by mihai on 2014-12-02.
  */
 public class Question {
+    private String answers[] = null;
+    private int[] rightAnswers = null;
+
+    public void Question() {
+
+    }
+
+    public String[] getAnswers() {
+        return this.answers;
+    }
+
+    public int[] getRightAnswers() {
+        return this.rightAnswers;
+    }
+
 }
--- Quizzard/app/src/main/java/se/bth/students/quizzard/Quiz.java
+++ Quizzard/app/src/main/java/se/bth/students/quizzard/Quiz.java
@@ -4,4 +4,37 @@ package se.bth.students.quizzard;
  * Created by mihai on 2014-12-02.
  */
 public class Quiz {
+    private Question[] questions = null;
+    private String course;
+    private String name;
+    private String author;
+    boolean finished = false; // if false this quiz has still to be completed with new questions, right answers etc.
+
+    public Question[] getQuestions() {
+        return this.questions;
+    }
+
+    public String getCourse() {
+        return this.course;
+    }
+
+    public String getName() {
+        return this.course;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    void setCourse(String course) {
+        this.course = course;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setAuthor(String author) {
+        this.author = author;
+    }
 }
