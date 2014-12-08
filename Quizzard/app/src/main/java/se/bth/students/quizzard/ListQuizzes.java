de20ac7bf6f29964e1864c6807581186ec1082da
Andreas Arnesson <Andreas_osby@hotmail.com>
2014-12-08 13:16:46 +0100

    kan skicka quiz obbject till lisquizzes

--- Quizzard/app/src/main/java/se/bth/students/quizzard/ListQuizzes.java
+++ Quizzard/app/src/main/java/se/bth/students/quizzard/ListQuizzes.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by mihai on 2014-12-02.
@@ -13,5 +16,9 @@ public class ListQuizzes extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_quizzes);
+
+        ArrayList<Quiz> quizzes =(ArrayList<Quiz>) getIntent().getSerializableExtra("quizzes");
+        Toast.makeText(getApplicationContext(), quizzes.get(0).getAuthor() + quizzes.get(1).getCourse(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),quizzes.get(1).getName(),Toast.LENGTH_SHORT).show();
     }
 }
